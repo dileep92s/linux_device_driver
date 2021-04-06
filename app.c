f08fc2cc32adb540501c2d635fc07cafaa7463b5
Dileep S <dileep92s@gmail.com>
2021-04-06 16:47:05 +0530

    Update app.c

--- app.c
+++ app.c
@@ -4,6 +4,7 @@
 int main()
 {
     int fd;
+    char msg[] = "hello driver!\n";
 
     fd = open("/dev/myDev", O_RDWR);
     if (fd < 0)
@@ -11,9 +12,15 @@ int main()
         perror("open");
     }
 
-    printf("pid = %d", getpid());
+    write(fd, msg, sizeof(msg));
+    perror("write");
 
-    ioctl(fd, getpid(), 12);
+    read(fd, msg, sizeof(msg));
+    perror("read");
+    printf("msg = %s\n", msg);
+
+    printf("pid = %d\n", getpid());
+    ioctl(fd, getpid(), 10);
 
     close(fd);
 
